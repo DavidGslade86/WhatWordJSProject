5414504823c92cc74be2f3dbaf95f804c342ecf7
David Greenslade <david.greenslade.cohort2211@gmail.com>
2023-02-22 17:54:35 -0800

    2.22.23 5.53pm Completed  UI

---
+++ wordApp.js
@@ -0,0 +1,5 @@
+//listen for submit
+document.querySelector("#loan-form").addEventListener('submit', function(e){
+    //Hide results
+    document.getElementById('results').style.display = 'none';
+
